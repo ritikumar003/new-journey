b46c4415a6fd137424bba9de08f0af756e16dc67
ritikumar003 <ritiking43@gmail.com>
2026-06-05 19:08:24 +0530

    dog

--- pyhton/type.py
+++ pyhton/type.py
@@ -1,5 +1,22 @@
-name="RITIK"
-age=77
+class dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
 
-print(name.lower())
-print(type(age))
+
+        pass
+    def bark(self):
+        print("woof woof")
+
+
+dog1 = dog("hell","street")
+dog1.bark()
+print(dog1.name)
+print(dog1.breed)
+
+
+
+dog2 = dog("nah","pug")
+dog2.bark()
+print(dog2.name)
+print(dog2.breed)
